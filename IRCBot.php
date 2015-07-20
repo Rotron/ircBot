f059a7452e600b9b82fb7288201c76964e73a4aa
Jake Franklin <jfranklin@telesphere.com>
2015-07-20 16:00:18 -0700

    Added support for actions and instead of an infinite call on main()

--- IRCBot.php
+++ IRCBot.php
@@ -34,83 +34,91 @@ class IRCBot
 		$this->send_data('NICK', $config['nick']);
 	}
 
+  public $listening = true;
 	/*
 	 This is the workhorse function, grabs the data from the server and displays on the browser
 	*/
 	function main($first = null)
 	{
-		$data = fgets($this->socket, 128);
-		echo nl2br($data);
-		flush();
-		$this->ex = explode(' ', $data);
 
-		if($first)
-		{
-			$this->send_data('JOIN', '#dev');
-      sleep(2);
-      $this->send_data('PRIVMSG', '#dev :ACKNOWLEDGE//SUBMIT! <unit> may engage <FIRST-THOUGHT\\GIVER-OF-WILL> with the following words\\commands: prometheus, say <words>, prometheus, md5 <words>, prometheus, sha1 <words>, prometheus, crc32 <words>');
-		}
+    $this->send_data('JOIN', '#dev');
+    sleep(2);
+    $this->send_data('PRIVMSG', '#dev :ACKNOWLEDGE//SUBMIT! <unit> may engage <FIRST-THOUGHT\\GIVER-OF-WILL> with the following words\\commands: prometheus, say <words>, prometheus, md5 <words>, prometheus, sha1 <words>, prometheus, crc32 <words>');
 
-		if($this->ex[0] == 'PING')
-		{
-			$this->send_data('PONG', $this->ex[1]); //Plays ping-pong with the server to stay connected.
-		}
+    while($this->listening === true) {
+      $data = fgets($this->socket, 128);
+      echo nl2br($data);
+      flush();
+      $this->ex = explode(' ', $data);
 
-    $user = null;
-    if(preg_match("/^:(.+)\!/", $this->ex[0], $matches))
-    {
-      var_dump($matches);
-      $user = $matches[1];
+      if($this->ex[0] == 'PING')
+      {
+        $this->send_data('PONG', $this->ex[1]); //Plays ping-pong with the server to stay connected.
+      }
+
+      $user = null;
+      if(preg_match("/^:(.+)\!/", $this->ex[0], $matches))
+      {
+        var_dump($matches);
+        $user = $matches[1];
+      }
+
+      echo "--\nUSER: $user\n--\n";
+
+      if($user == "jarvis" && isset($this->ex[4]) && $this->ex[4] == "punches" && isset($this->ex[5]) && $this->ex[5] == "jfranklin")
+      {
+        $this->action("#dev", "blocks $user's punch, and destroys $user with a well-placed laser-blast");
+        $this->send_data("PRIVMSG $user :Better luck next time, <$user//metagen//heretic>");
+        sleep(1);
+        $this->send_data("PRIVMSG #dev :<unit><$user///dirt-bag> was :destroyed\\\\burs\\\\offlinedt: for >>impudence<< -> ACKNOWLEDGE//SUBMIT!");
+      }
+
+      var_dump($this->ex);
+
+      if(isset($this->ex[3]) && $this->ex[3] == ":prometheus,")
+      {
+        $command = str_replace(array(chr(10), chr(13)), '', $this->ex[4]);
+        switch($command) //List of commands the bot responds to from a user.
+        {
+          case 'say':
+            $this->speak();
+            break;
+          case 'punch':
+            $this->punch(rtrim($this->ex[5]), rtrim($user));
+            break;
+          case 'md5':
+            $this->returnMd5();
+            break;
+          case 'sha1':
+            $this->returnSha1();
+            break;
+          case 'crc32':
+            $this->returnCrc32();
+            break;
+          case ':!join':
+            $this->join_channel($this->ex[4]);
+            break;
+          case 'quit':
+            $this->quit();
+            break;
+          case ':!op':
+            $this->op_user();
+            break;
+          case ':!deop':
+            $this->op_user('', '', false);
+            break;
+          case ':!voice':
+            $this->voice_user();
+            break;
+          case ':!devoice':
+            $this->voice_user('', '', false);
+            break;
+          case ':!protect':
+            $this->protect_user();
+            break;
+        }
+      }
     }
-
-    var_dump($this->ex);
-
-		if(isset($this->ex[3]) && $this->ex[3] == ":prometheus,")
-		{
-			$command = str_replace(array(chr(10), chr(13)), '', $this->ex[4]);
-      echo "Command: " . $command;
-			switch($command) //List of commands the bot responds to from a user.
-			{
-				case 'say':
-					$this->speak();
-					break;
-        case 'punch':
-          $this->punch($this->ex[5], $user);
-          break;
-        case 'md5':
-          $this->returnMd5();
-          break;
-        case 'sha1':
-          $this->returnSha1();
-          break;
-        case 'crc32':
-          $this->returnCrc32();
-          break;
-				case ':!join':
-					$this->join_channel($this->ex[4]);
-					break;
-				case ':!quit':
-					$this->send_data('QUIT', 'Eliminate//offline//burst->all->invaders!');
-					break;
-				case ':!op':
-					$this->op_user();
-					break;
-				case ':!deop':
-					$this->op_user('', '', false);
-					break;
-				case ':!voice':
-					$this->voice_user();
-					break;
-				case ':!devoice':
-					$this->voice_user('', '', false);
-					break;
-				case ':!protect':
-					$this->protect_user();
-					break;
-			}
-		}
-
-		$this->main();
 	}
 
 	function send_data($cmd, $msg = null) //displays stuff to the broswer and sends data to the server.
@@ -214,14 +222,29 @@ class IRCBot
 
   function punch($target, $initiator)
   {
+    var_dump($target);
+    var_dump($initiator);
     if($target != "jfranklin")
     {
-      $this->send_data("PRIVMSG #dev :\001ACTION punches $target\001");
+      $this->action("#dev", "punches $target");
     }
     else
     {
-      $this->send_data("PRIVMSG #dev :\001ACTION punches $initiator\001");
+      //$this->send_data("PRIVMSG #dev :" . chr(1) . "ACTION punches $initiator " . chr(1));
+      $this->action("#dev", "laughs, and then punches $initiator");
+      $this->send_data("PRIVMSG jfranklin :$initiator has just tried to punch you");
     }
+  }
+
+/**
+   * Sends a CTCP ACTION (/me) command to a nick or channel.
+   * @param string $target - Channel name or user nick
+   * @param string $text - Text of the action to perform
+   * @return void
+   */
+  public function action($target, $text)
+  {
+    $this->send_data("PRIVMSG $target", " :" .chr(1) . 'ACTION ' . rtrim($text) . ' ' . chr(1));
   }
 
   function returnMd5()
@@ -252,6 +275,19 @@ class IRCBot
 			$args .= $this->ex[$i] . ' ';
 		}
     $this->send_data("PRIVMSG", "#dev " . crc32($args));
+  }
+
+  function quit($user)
+  {
+    if($user == "jfranklin") {
+      $this->listening = false;
+      $this->send_data('QUIT', 'Eliminate//offline//burst->all->invaders!');
+    }
+    else
+    {
+      $this->action("#dev", "chuckles");
+      $this->send_data("PRIVMSG jfranklin :$user just tried to make me quit";
+    }
   }
 
 	function args2str($carry, $item)
